36c79a31ed9f82dc4d80e6c637b3fc2097f98590
RashmiBasnet <rashmibasnet2019@gmail.com>
2026-01-03 23:30:36 +0545

    Finished Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 import { PORT } from "./config";
 import { connectDatabase } from "./database/mongoose";
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.PORT);
+
+import authRoutes from "./routes/auth.routes";
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+
+async function start() {
+    await connectDatabase();
+
+    app.listen(PORT, () => {
+        console.log(`Server: http:localhost:${PORT}`);
+    })
+}
+start().catch((error) => console.log(error));
