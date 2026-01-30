11ebe2703e9ad1d527504b8d223b7763c7018bbe
RashmiBasnet <rashmibasnet2019@gmail.com>
2026-01-30 22:54:45 +0545

    Created User Routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,13 @@
+import { UserController } from "../controllers/user.controller";
+import { Router } from "express";
+import { authorizedMiddleware } from "../middleware/authorization.middleware";
+import { uploads } from "../middleware/upload.middleware";
+
+const router = Router();
+let userController = new UserController();
+
+router.get("/profile", authorizedMiddleware, userController.getProfile);
+router.put("/update-profile", authorizedMiddleware, uploads.single("profile"), userController.updateProfile);
+router.put("/profile/upload", authorizedMiddleware, uploads.single("profile"), userController.uploadProfilePicture);
+
+export default router;
